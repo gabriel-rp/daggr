75d57638bc8009edb97fe059052fc5a7fa00ad75
Gabriel Richard Pereira <gabriel.pereira@picpay.com>
2021-12-11 20:52:20 -0300

    fixes test

--- tests/core/scripts/step2.py
+++ tests/core/scripts/step2.py
@@ -1,7 +1,1 @@
-from pathlib import Path
-
-Path("/Users/gabriel.pereira/Documents/mle/repos/daggr/tests/core/scripts/log").touch()
-with open(
-    "/Users/gabriel.pereira/Documents/mle/repos/daggr/tests/core/scripts/log", "a"
-) as f:
-    f.write("step2\n")
+print("step2\n")
