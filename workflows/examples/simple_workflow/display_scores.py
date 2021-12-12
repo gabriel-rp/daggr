72e16cdf4246fd3574a2c3f0c9d59111feb196d4
Gabriel Richard Pereira <gabriel.pereira@picpay.com>
2021-12-12 20:17:03 -0300

    linting and formatting

--- workflows/examples/simple_workflow/display_scores.py
+++ workflows/examples/simple_workflow/display_scores.py
@@ -1,9 +1,11 @@
 from typing import Any, Dict
 
-from daggr.core.decorators import inputs, output
+from daggr.core.decorators import inputs
+
 
 @inputs()
 def display(inputs: Dict[str, Any], parameters: Dict[str, Any]):
     print(inputs["approved"])
+
 
 display()
